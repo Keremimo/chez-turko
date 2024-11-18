a90382ad5b8de064ba7a482c9d022be790d2d035
Keremimo <nyaa@live.com>
2024-11-18 11:47:53 +0100

    Add hero landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
 const Home = () => {
   return (
-    <>
-      <h1>Welcome to home page!</h1>
-    </>
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+      }}>
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold text-white">Chez Turko</h1>
+          <p className="mb-5 text-white">
+            Welcome to Chez Turko, the French Turkish fusion kitchen. Here you will find gourmet selections that are made by our three star chef, <span className="font-bold">Ahmet LeBoyard</span>! Delivery only! Order now!
+          </p>
+          <button className="btn btn-primary">Check Menu</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
