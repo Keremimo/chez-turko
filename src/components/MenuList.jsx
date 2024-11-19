155f075721205f203cdc6d82dcc85c89082d933a
Keremimo <nyaa@live.com>
2024-11-19 11:42:20 +0100

    Forward menu item data to menu list

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -1,8 +1,13 @@
-const MenuList = () => {
+import FoodItem from "./FoodItem"
+const MenuList = ({ list }) => {
+  console.log(list)
+  const foodItemList = list.menu.map((item, index) => (
+    <FoodItem key={index} details={item} />
+  ))
   return (
-    <>
-
-    </>
+    <div className="join-item flex min-w-12 flex-wrap">
+      {foodItemList}
+    </div>
   )
 }
 
