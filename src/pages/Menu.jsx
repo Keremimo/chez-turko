c7009598c037ccc2e0f91acab8947004178613e4
Keremimo <nyaa@live.com>
2024-11-20 11:54:31 +0100

    Add session storage to shopping cart

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -3,16 +3,23 @@ import jsonData from '../assets/menu.json'
 import { MenuList, Cart } from '../components'
 
 const Menu = () => {
-  const [shoppingCart, setShoppingCart] = useState([])
+  const [shoppingCart, setShoppingCart] = useState(() => {
+    if (sessionStorage.getItem('react-shopping-cart')) {
+      return JSON.parse(sessionStorage.getItem('react-shopping-cart'))
+    }
+    else return []
+
+  })
   const handleCartAdd = (obj) => {
     const newCart = structuredClone(shoppingCart)
     newCart.push(obj)
+    sessionStorage.setItem('react-shopping-cart', JSON.stringify(newCart))
     setShoppingCart(newCart)
   }
   return (
     <div className='join flex'>
-      <MenuList list={jsonData} cartModify={handleCartAdd} />
       <Cart cart={shoppingCart} />
+      <MenuList list={jsonData} cartModify={handleCartAdd} />
     </div>
   )
 }
