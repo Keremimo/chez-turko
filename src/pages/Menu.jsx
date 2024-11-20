9c2d4a63baa86120c843c33f5d7a6fb20bc0c949
Keremimo <nyaa@live.com>
2024-11-20 11:08:38 +0100

    Add shopping cart handler

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -4,10 +4,15 @@ import { MenuList, Cart } from '../components'
 
 const Menu = () => {
   const [shoppingCart, setShoppingCart] = useState([])
+  const handleCartAdd = (obj) => {
+    const newCart = structuredClone(shoppingCart)
+    newCart.push(obj)
+    setShoppingCart(newCart)
+  }
   return (
     <div className='join flex'>
-      <MenuList list={jsonData} />
-      <Cart />
+      <MenuList list={jsonData} cartModify={handleCartAdd} />
+      <Cart cart={shoppingCart} />
     </div>
   )
 }
