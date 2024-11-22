da7b98a7415bf0aa070077740802865f6ddb098b
Keremimo <nyaa@live.com>
2024-11-22 12:30:49 +0100

    Add all order page logic incl. modal, prices, info

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,15 +1,32 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
-const Order = ({ pass }) => {
-  console.log(pass)
+const Order = () => {
+  const navigate = useNavigate()
   const [cart, setCart] = useState(sessionStorage.getItem('react-shopping-cart') ? JSON.parse(sessionStorage.getItem('react-shopping-cart')) : [])
   const initialPrice = 0
   const totalPrice = (cart.reduce((accumulator, obj) => accumulator + (obj.price * obj.quantity), initialPrice)) / 100
 
-  const handleAddition = 'hi'
-  const handleRemoval = 'hi'
+  const handleAddition = (e) => {
+    const newCart = [...cart]
+    const { value: index } = e.target
+    newCart[index].quantity += 1
+    sessionStorage.setItem('react-shopping-cart', JSON.stringify(newCart))
+    setCart(newCart)
+  }
+
+  const handleRemoval = (e) => {
+    const newCart = [...cart]
+    const { value: index } = e.target
+    newCart[index].quantity -= 1
+    if (newCart[index].quantity <= 0) {
+      newCart.splice(index, 1)
+    }
+    sessionStorage.setItem('react-shopping-cart', JSON.stringify(newCart))
+    setCart(newCart)
+  }
   const orderList = cart.map((item, index) => (
-    <tr key={index}>
+    <tr key={index} className="text-center">
       <td>
         <div className="mask mask-squircle h-12 w-12">
           <img
@@ -23,7 +40,7 @@ const Order = ({ pass }) => {
 
           </div>
           <div className="mr-auto">
-            <p className="font-bold text-left">{item.name}</p>
+            <p className="font-bold">{item.name}</p>
           </div>
         </div>
       </td>
@@ -31,14 +48,34 @@ const Order = ({ pass }) => {
         {item.ingredients.join(', ')}
         <br />
       </td>
-      <td>{item.quantity}</td>
+      <td className="items-center">
+        <div className="flex">
+          <button onClick={handleRemoval} value={index} className="btn btn-error btn-xs mr-2">-</button>
+          {item.quantity}
+          <button onClick={handleAddition} value={index} className="btn btn-success btn-xs ml-2">+</button>
+        </div>
+      </td>
+      <td>{(item.price * item.quantity) / 100}</td>
     </tr>
   ))
 
   return (
     <div className="flex flex-col">
+      <dialog id="finishorder" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Order placed!</h3>
+          <p className="py-4">Wait time: 20 business weeks. Hope you enjoy!</p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
       <div className="flex w-full flex-col lg:flex-row">
-        <div className="card bg-base-300 rounded-box flex h-full w-1/2 place-items-center">
+        <div className="card bg-base-300 rounded-box flex h-full md:w-full w-1/2 place-items-center">
+          <h2 className="text-2xl font-bold m-4">Enter your details:</h2>
           <form className="m-4" action="">
             <div className="flex" >
               <label className="input input-bordered flex items-center gap-2">
@@ -69,23 +106,26 @@ const Order = ({ pass }) => {
           </form>
         </div>
         <div className="divider lg:divider-horizontal"></div>
-        <div className="card bg-base-300 rounded-box grid h-full w-1/2 place-items-baseline content-center p-5">
-          <table className="table">
-            <thead>
-              <tr>
+        <div className="card bg-base-300 rounded-box grid h-80 w-1/2 md:w-full place-items-baseline content-start overflow-scroll">
+          <table className="table bg-base-300">
+            <thead className="sticky top-0 bg-base-300 z-20">
+              <tr className="bg-base-300">
                 <th></th>
                 <th className="text-center">Item</th>
                 <th className="text-center">Ingredients</th>
                 <th className="text-center">Quantity</th>
+                <th className="text-center">Price</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className="h-96">
               {orderList}
             </tbody>
           </table>
         </div>
       </div>
-      <button className="btn btn-primary mx-auto text-center">Place Order</button>
+      <p className="text-xl text-center font-bold">Total: {totalPrice}</p>
+      <button onClick={() => document.getElementById('finishorder').showModal()} className="btn btn-primary mx-auto text-center">Place Order</button>
+      <button onClick={() => { navigate(-1) }} className="btn btn-error mx-auto text-center">Go Back</button>
     </div>
   )
 }
