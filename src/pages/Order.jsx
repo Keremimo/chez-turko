329543305879fae7d50920096bbb9db12d9eccfe
Keremimo <nyaa@live.com>
2024-11-22 10:45:27 +0100

    Add cart as variable to order page

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,6 +1,18 @@
-const Order = () => {
+const Order = ({ pass }) => {
+  console.log(pass)
+  const cart = sessionStorage.getItem('react-shopping-cart') ? JSON.parse(sessionStorage.getItem('react-shopping-cart')) : []
+
+
   return (
-    <> </>
+    <div className="flex w-full flex-col lg:flex-row">
+      <div className="card bg-base-300 rounded-box grid h-32 flex-grow place-items-center">
+
+      </div>
+      <div className="divider lg:divider-horizontal">OR</div>
+      <div className="card bg-base-300 rounded-box grid h-32 flex-grow place-items-center">
+
+      </div>
+    </div>
   )
 }
 
